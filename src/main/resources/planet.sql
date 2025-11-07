f3ade44865456d384e50315453ceb2b98f149fa7
SamuelLotty <samlotty@gmail.com>
2025-11-07 10:30:42 +0000

    Initial commit

---
+++ src/main/resources/planet.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Planet (
+ planet_id INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(50),
+ type VARCHAR(50),
+ radius_km INT,
+ mass_kg INT,
+ orbital_period_days INT
+);
---
+++ src/test/java/org/example/springframeworkassignment/SpringFrameworkAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springframeworkassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
