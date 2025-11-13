2cf9d08710197ba4651fa487b0dec2544043bd80
SamuelLotty <samlotty@gmail.com>
2025-11-13 12:15:12 +0000

    Service java files made for Moon Planet and User.

--- src/main/java/org/example/springframeworkassignment/service/MoonService.java
+++ src/main/java/org/example/springframeworkassignment/service/MoonService.java
@@ -1,7 +1,10 @@
+package org.example.springframeworkassignment.service;
+
 import org.example.springframeworkassignment.daos.entities.Moon;
 
 public interface MoonService
 {
     Moon save (Moon moon);
+
 
 }
