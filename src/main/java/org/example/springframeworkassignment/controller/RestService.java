5ac005b1cf1e903cbc9b6fc9bb82229c1a245ac1
StanV000 <stanimir.vasilev@mycit.ie>
2025-11-24 12:27:17 +0000

    GetMapping example

--- src/main/java/org/example/springframeworkassignment/controller/RestService.java
+++ src/main/java/org/example/springframeworkassignment/controller/RestService.java
@@ -2,10 +2,14 @@ package org.example.springframeworkassignment.controller;
 
 
 import lombok.AllArgsConstructor;
+import org.example.springframeworkassignment.daos.entities.Planet;
 import org.example.springframeworkassignment.service.MoonService;
 import org.example.springframeworkassignment.service.PlanetService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -14,5 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestService {
     private MoonService moonService;
     private PlanetService planetService;
+
+    @GetMapping(headers = "/", path = "")
+    public List<Planet> getAllPlanets() {
+        return planetService.getAllPlanets();
+    }
+
+
+
 
 }
