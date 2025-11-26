0782a1aa4da43baca176a1f27d387d355d84e2d6
SamuelLotty <samlotty@gmail.com>
2025-11-26 11:34:07 +0000

    More functionality added to RestService.

--- src/main/java/org/example/springframeworkassignment/controller/RestService.java
+++ src/main/java/org/example/springframeworkassignment/controller/RestService.java
@@ -4,15 +4,12 @@ package org.example.springframeworkassignment.controller;
 import lombok.AllArgsConstructor;
 import org.example.springframeworkassignment.daos.entities.Moon;
 import org.example.springframeworkassignment.daos.entities.Planet;
-import org.example.springframeworkassignment.dto.Mappers;
-import org.example.springframeworkassignment.dto.PlanetDTO;
+import org.example.springframeworkassignment.exceptions.NotFoundException;
 import org.example.springframeworkassignment.service.MoonService;
 import org.example.springframeworkassignment.service.PlanetService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.yaml.snakeyaml.events.Event;
+import org.example.springframeworkassignment.service.PlanetServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,10 +35,19 @@ public class RestService {
     }
 
     @GetMapping("/{id}")
-    public Optional<Planet> findbyId(@PathVariable("id") int id){
+    public Optional<Planet> findPlanetId(@PathVariable("id") int id){
         return planetService.getPlanetById(id);
     }
 
+    @GetMapping("/{id}")
+    public Optional<Moon> findMoonId(@PathVariable("id") int id){
+        return moonService.findById(id);
+    }
 
-
+    @DeleteMapping({"/{id}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePlanet(@PathVariable("id") int id) throws NotFoundException
+    {
+        //PlanetService.deletePlanetById(id);
+    }
 }
