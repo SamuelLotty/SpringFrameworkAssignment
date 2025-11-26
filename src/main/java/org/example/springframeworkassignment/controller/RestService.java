5de27bda6df7de894948b5e6c768be6b7a9fd924
SamuelLotty <samlotty@gmail.com>
2025-11-26 11:22:24 +0000

    Functionality added for RestService.

--- src/main/java/org/example/springframeworkassignment/controller/RestService.java
+++ src/main/java/org/example/springframeworkassignment/controller/RestService.java
@@ -4,13 +4,18 @@ package org.example.springframeworkassignment.controller;
 import lombok.AllArgsConstructor;
 import org.example.springframeworkassignment.daos.entities.Moon;
 import org.example.springframeworkassignment.daos.entities.Planet;
+import org.example.springframeworkassignment.dto.Mappers;
+import org.example.springframeworkassignment.dto.PlanetDTO;
 import org.example.springframeworkassignment.service.MoonService;
 import org.example.springframeworkassignment.service.PlanetService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -19,6 +24,7 @@ import java.util.List;
 public class RestService {
     private MoonService moonService;
     private PlanetService planetService;
+
     //Ask about Headers = and path =
 
     @GetMapping(headers = "/", path = "")
@@ -31,6 +37,10 @@ public class RestService {
         return  moonService.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Optional<Planet> findbyId(@PathVariable("id") int id){
+        return planetService.getPlanetById(id);
+    }
 
 
 
