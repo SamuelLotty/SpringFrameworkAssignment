22cf9e6c3605b831f9592a6b2273cbb92f1c5de3
SamuelLotty <samlotty@gmail.com>
2025-12-09 08:45:31 +0000

    updated REST service endpoints. Consistent mapping urls

--- src/main/java/org/example/springframeworkassignment/controller/RestService.java
+++ src/main/java/org/example/springframeworkassignment/controller/RestService.java
@@ -15,34 +15,37 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
 public class RestService {
     private MoonService moonService;
     private PlanetService planetService;
+
     @Operation(summary = "Lists all planets",description = "Retrieve a list of all planets within the database")
     @GetMapping("/planets")
     public List<PlanetDTO> getAllPlanets() {
         return planetService.getAllPlanets();
     }
+
     @Operation(summary = "List of all moons",description = "Retrieve a list of all moons within the database")
-    @GetMapping("/moons/")
+    @GetMapping("/moons")
     public List<MoonDTO> getAllMoons(){
         return  moonService.findAllMoons();
     }
-    @Operation(summary = "Find a planet by Id", description = "Retrieve a list of planets based on the ID")
-    @GetMapping("/planet/{id}")
 
+    @Operation(summary = "Find a planet by Id", description = "Retrieve a list of planets based on the ID")
+    @GetMapping("/planets/{id}")
     public Optional<Planet> findPlanetId(@PathVariable("id") int id){
         return planetService.getPlanetById(id);
     }
+
     @Operation(summary = "Find a moon by Id",description = "Retrieve a list of moons based on the ID")
     @GetMapping("/moons/{id}")
     public MoonDTO findMoonId(@PathVariable("id") int id){
         return moonService.findById(id);
     }
+
     @Operation(summary = "Delete planet by ID", description = "Deletes a planet based on the ID")
     @DeleteMapping({"/planets/{id}"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -50,6 +53,7 @@ public class RestService {
     {
         planetService.deletePlanetById(id);
     }
+
     @Operation(summary = "Delete moon by ID", description = "Deletes a planet based on the ID")
     @DeleteMapping({"/moons/{id}"})
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -59,19 +63,19 @@ public class RestService {
     }
 
     @Operation(summary = "Update details of planet",description = "Updates details of a specific planet based on ID")
-    @PatchMapping({"/planet"})
+    @PatchMapping({"/planets"})
     public void updatePlanet(@RequestBody PlanetDTO planetDTO) {
        planetService.updatePlanet(planetDTO);
     }
 
     @Operation(summary = "List moons by planet",description = "List the moons for each planet based on ID")
-    @GetMapping("/planet/moons/")
+    @GetMapping("/planets/moons/")
     public List<MoonDTO> getMoonsByPlanet(@RequestParam("id") int id){
         return moonService.findAllByPlanet(id);
     }
 
     @Operation(summary = "Retrieve planets based on type",description = "Retrieve planets based on what type of planet they are")
-    @GetMapping("/planet/")
+    @GetMapping("/planets/")
     public List<PlanetDTO> getPlanetsByType(@RequestParam("type") String type){
         return planetService.getPlanetsByType(type);
     }
